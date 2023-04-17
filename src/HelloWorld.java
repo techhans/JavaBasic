d7a2162d7c041aa2af8ae794aee99808febbdc6c
nicehans <nicehans@gmail.com>
2023-04-17 19:41:58 +0900

    commit update

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
 		System.out.println("Hello World!"); 
-	}
+	} 
 
 }
