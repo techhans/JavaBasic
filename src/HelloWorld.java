74fae10160ae6c16a0e312e6e8668f5eb9f730f3
nicehans <nicehans@gmail.com>
2023-04-18 09:21:05 +0900

    commit update with .gitignore

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
 		System.out.println("Hello World!"); 
+		
+		final int MAX = 100;
+		
+		
+
 	} 
 
 }
