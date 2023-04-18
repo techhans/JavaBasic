0cd4772347e3db667720d7da7ee85bebb3e617f0
nicehans <nicehans@gmail.com>
2023-04-18 16:06:15 +0900

    commit update

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,18 @@
 
 public class HelloWorld {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
-		System.out.println("Hello World!"); 
-		
-		final int MAX = 100;
+		System.out.println("Hello World!");
 		
 		
+		int value;
+		int result = value + 10;
+		// error: variable value might not have been initialized
+		System.out.println("[DEBUG] result:"+result);
 
-	} 
 
+		int add(int a, int b)
+	}
 }
+
+
