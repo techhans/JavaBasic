710481ebbed7e100fbe9ae0b13a6409475e039fe
nicehans <nicehans@gmail.com>
2023-04-19 09:25:37 +0900

    commit update from javabasic

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,15 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
 		System.out.println("Hello World!");
-		
-		
-		int value;
-		int result = value + 10;
-		// error: variable value might not have been initialized
-		System.out.println("[DEBUG] result:"+result);
-
-
-		int add(int a, int b)
 	}
 }
 
